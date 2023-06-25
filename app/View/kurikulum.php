ffa6b2dbde54c6919eb2af2f802b58200c5f78b4
Lenovo <fathin2000018359@webmail.com>
2023-06-25 13:31:31 +0700

    Prestasi

--- app/View/kurikulum.php
+++ app/View/kurikulum.php
@@ -10,8 +10,7 @@
             <!--                <a href="#top" class="navbar-icon bi-person smoothscroll"></a>-->
             <!--            </div>-->
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -22,8 +21,7 @@
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"
-                           data-bs-toggle="dropdown" aria-expanded="false">Profil</a>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profil</a>
 
                         <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
                             <li><a class="dropdown-item" href="/struktur-organisasi">Struktur Organisasi</a></li>
@@ -63,11 +61,11 @@
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="/index">Homepage</a></li>
 
-                            <li class="breadcrumb-item active" aria-current="page">Kurikulum</li>
+                            <li class="breadcrumb-item active" aria-current="page">Visi dan Misi</li>
                         </ol>
                     </nav>
 
-                    <h2 class="text-white">Kurikulum</h2>
+                    <h2 class="text-white">Visi dan Misi</h2>
                 </div>
 
             </div>
@@ -79,202 +77,78 @@
         <div class="container">
             <div class="row">
 
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th rowspan="2">Mata Pelajaran</th>
-                        <th colspan="3">Alokasi Waktu</th>
-                    </tr>
-                    <tr>
-                        <th>Kelas VII</th>
-                        <th>Kelas VIII</th>
-                        <th>Kelas IX</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Kelompok A</td>
-                        <td colspan="4"></td>
-                    </tr>
-                    <tr>
-                        <td>Pendidikan Agama</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>a. Al Quran Hadits</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>b. Akidah Akhlak</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>c. Fiqih</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>d. Sejarah Kebudayaan Islam</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>Pendidikan Pancasila dan Kewarganegaraan</td>
-                        <td>3</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Bahasa Indonesia</td>
-                        <td>6</td>
-                        <td>6</td>
-                        <td>6</td>
-                    </tr>
-                    <tr>
-                        <td>Bahasa Arab</td>
-                        <td>3</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Matematika</td>
-                        <td>5</td>
-                        <td>5</td>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <td>Ilmu Pengetahuan Alam</td>
-                        <td>5</td>
-                        <td>5</td>
-                        <td>5</td>
-                    </tr>
-                    <tr>
-                        <td>Ilmu Pengetahuan Sosial</td>
-                        <td>4</td>
-                        <td>4</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>Bahasa Inggris</td>
-                        <td>4</td>
-                        <td>4</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>Kelompok B</td>
-                        <td colspan="4"></td>
-                    </tr>
-                    <tr>
-                        <td>Seni Budaya</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>Pendidikan Jasmani, Olahraga dan Kesehatan</td>
-                        <td>3</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Prakarya dan / atau Informatika*)</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>TIK</td>
-                        <td>-</td>
-                        <td>2</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <th>Jumlah</th>
-                        <th>47</th>
-                        <th>47</th>
-                        <th>47</th>
-                    </tr>
-                    <tr>
-                        <th>Muatan Lokal</th>
-                        <td colspan="4"></td>
-                    </tr>
-                    <tr>
-                        <td>Praktek Ibadah</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <th>Jumlah</th>
-                        <th>50</th>
-                        <th>48</th>
-                        <th>48</th>
-                    </tr>
-                    <tr>
-                        <th>Bimbingan dan Pelayanan</th>
-                        <td colspan="4"></td>
-                    </tr>
-                    <tr>
-                        <td>BP/BK</td>
-                        <td>1</td>
-                        <td>-</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <td>TIK</td>
-                        <td>-</td>
-                        <td>1</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <td>BTQ</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <th>Pengembangan Diri</th>
-                        <td colspan="4"></td>
-                    </tr>
-                    <tr>
-                        <td>PRAMUKA dan PMR</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>Olah Raga</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>KIR (MTK, IPA)</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>Seni (Drumb band, Tilawah)</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>2</td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div class="col-lg-8 col-12 mt-3 mx-auto">
+                    <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                        <div class="d-flex">
+                            <div class="custom-block-topics-listing-info d-flex">
+                                <div>
+                                    <h5 class="mb-2 text-center">Kurikulum (K13)</h5>
 
+                                    <p class="mb-0">Kurikulum 2013 merupakan kurikulum yang lebih mengutamakan pemahaman, skill, dan pendidikan yang berkarakter, siswa dituntut untuk lebih aktif dalam proses pembelajaran, siswa dituntut untuk paham atas materi serta siswa harus aktif berdikusi dan mampu berpresentasi serta memiliki sopan santu dan disiplin yang tinggi.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="custom-block custom-block-topics-listing bg-white shadow-lg mb-5">
+                        <div class="d-flex">
+                            <div class="custom-block-topics-listing-info d-flex">
+                                <div>
+                                    <h5 class="mb-2 text-center">Kurikulum</h5>
+
+                                        <div class="accordion" id="accordionExample">
+                                            <div class="accordion-item">
+                                                <h2 class="accordion-header" id="headingOne">
+                                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                        Prestasi Siswa
+                                                    </button>
+                                                </h2>
+
+                                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                    <div class="accordion-body">
+                                                        Juara 1 MTK KSM Tk. Kabupaten Sambas ( 2017 )
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="accordion-item">
+                                                <h2 class="accordion-header" id="headingTwo">
+                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                        Prestasi Guru
+                                                    </button>
+                                                </h2>
+
+                                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                                    <div class="accordion-body">
+                                                        Juara 1 MTK KSM Tk. Kabupaten Sambas ( 2017 )
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="accordion-item">
+                                                <h2 class="accordion-header" id="headingThree">
+                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                        Prestasi Sekolah
+                                                    </button>
+                                                </h2>
+
+                                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                                    <div class="accordion-body">
+                                                        Juara 1 MTK KSM Tk. Kabupaten Sambas ( 2017 )
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
-
-        </div>
         </div>
     </section>
 </main>
@@ -288,10 +162,7 @@
             </div>
 
             <div class="col-lg-5 col-12 mb-4 mb-lg-0">
-                <iframe class="google-map"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.832108930004!2d109.07498579932798!3d1.189849675765968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31e49c2fbdf3d491%3A0x91e764f45793cc44!2sMTs.%20NEGERI%202%20SAMBAS!5e0!3m2!1sid!2sid!4v1686462693971!5m2!1sid!2sid"
-                        width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.832108930004!2d109.07498579932798!3d1.189849675765968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31e49c2fbdf3d491%3A0x91e764f45793cc44!2sMTs.%20NEGERI%202%20SAMBAS!5e0!3m2!1sid!2sid!4v1686462693971!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
             <div class="col-lg-3 col-md-4 col-6 mb-4 mb-lg-0">
@@ -336,8 +207,7 @@
 
         </div>
         <div class="row">
-            <p class="copyright-text mt-lg-5 mt-4 text-center text-white">Copyright © 2048 Topic Listing Center. All
-                rights reserved.</p>
+            <p class="copyright-text mt-lg-5 mt-4 text-center text-white">Copyright © 2048 Topic Listing Center. All rights reserved.</p>
         </div>
     </div>
 </footer>
