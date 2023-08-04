346410561219131e4a24a6aa1cb29ae930c0ce74
Lenovo <fathin2000018359@webmail.com>
2023-08-04 21:46:48 +0700

    bagian ppdb

--- app/View/ppdb.php
+++ app/View/ppdb.php
@@ -1,8 +1,9 @@
 <main>
 
 
-    <section class="bg-image" style="height: 100vh; width: 100vw; display: flex;">
-        <img src="images/.png" style="max-width: 100%; max-height: 100%;">
+    <section style="height: 100vh; width: 90vw;">
+        <img src="images/2d1k8d1tf85s_ppdb.jpg"
+             class="d-block aspect-ratio-img">
     </section>
 
 
@@ -20,7 +21,6 @@
 
                     <div class="container">
                         <div class="row">
-
                             <div class="col-lg-10 col-12 mx-auto mw-100">
                                 <div class="timeline-container">
                                     <ul class="vertical-scrollable-timeline" id="vertical-scrollable-timeline">
@@ -30,9 +30,7 @@
 
                                         <li>
                                             <h4 class="text-black mb-3">Pendaftaran</h4>
-
                                             <p class="text-black">15 - 23 Juni</p>
-
                                             <div class="icon-holder">
                                                 <i class="bi-search"></i>
                                             </div>
@@ -40,27 +38,23 @@
 
                                         <li>
                                             <h4 class="text-black mb-3">Pengumuman Hasil</h4>
-
                                             <p class="text-black">Tanggal</p>
-
                                             <div class="icon-holder">
                                                 <i class="bi-bookmark"></i>
                                             </div>
                                         </li>
+
                                         <li>
                                             <h4 class="text-black mb-3">Daftar Ulang</h4>
-
                                             <p class="text-black">Tanggal</p>
-
                                             <div class="icon-holder">
                                                 <i class="bi-bookmark"></i>
                                             </div>
                                         </li>
+
                                         <li>
                                             <h4 class="text-black mb-3">Awal Tahun Ajaran Baru</h4>
-
                                             <p class="text-black">Tanggal</p>
-
                                             <div class="icon-holder">
                                                 <img src="/images/Seni.png">
                                             </div>
